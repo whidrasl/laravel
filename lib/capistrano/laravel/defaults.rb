2bf69a5b6b9c79a20beae84f9a9e60eb52b30915
Korviakov Andrey <a.korvyakov@netbox.ru>
2015-09-16 13:43:17 +0300

    Change naming

--- lib/capistrano/laravel/defaults.rb
+++
@@ -1,8 +0,0 @@
-set :laravel_roles, :all
-set :laravel_artisan_flags, '--env=production'
-set :laravel_server_user, "deploy"
-
-set :linked_dirs, ['vendor', 'storage/app', 'storage/logs', 'storage/framework/cache', 'storage/framework/sessions', 'storage/framework/views', 'bootstrap/cache']
-
-set :file_permissions_paths, fetch(:linked_dirs)
-set :file_permissions_users, [fetch(:laravel_server_user)]
