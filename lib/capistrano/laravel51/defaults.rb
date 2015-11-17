d3e650abeff78f96cf0b41439360bcbc21a4a321
Korviakov Andrey <a.korvyakov@netbox.ru>
2015-11-17 13:26:12 +0300

    fix linked_dirs

--- lib/capistrano/laravel51/defaults.rb
+++ lib/capistrano/laravel51/defaults.rb
@@ -2,7 +2,8 @@ set :laravel_roles, :all
 set :laravel_artisan_flags, '--env=production'
 set :laravel_server_user, "deploy"
 
-set :linked_dirs, fetch(:linked_dirs) + ['vendor', 'storage/app', 'storage/logs', 'storage/framework/cache', 'storage/framework/sessions', 'storage/framework/views', 'bootstrap/cache']
+set :linked_dirs, []
+set :linked_dirs, fetch(:linked_dirs) | ['vendor', 'storage/app', 'storage/logs', 'storage/framework/cache', 'storage/framework/sessions', 'storage/framework/views', 'bootstrap/cache']
 
 set :file_permissions_paths, fetch(:linked_dirs)
 set :file_permissions_users, [fetch(:laravel_server_user)]
